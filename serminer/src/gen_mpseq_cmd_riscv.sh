6076f29852d6c6bb0272733694a9a56813587780
Karthik Swaminathan <kvswamin@us.ibm.com>
2020-01-29 11:49:06 -0500

    Moved env command to top of file

--- serminer/src/gen_mpseq_cmd_riscv.sh
+++ serminer/src/gen_mpseq_cmd_riscv.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Copyright 2020 IBM Corporation
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#!/usr/bin/env bash
 
 set -e
 
